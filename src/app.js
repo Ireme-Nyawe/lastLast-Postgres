1dc57671971171aa810b26901d2ec998c3d3f681
Ireme-Nyawe <akimanaj41@gmail.com>
2023-11-14 17:59:01 +0200

    ensure server for documentation

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const options = {
         },
         
         servers :[{
-            url: 'http://localhost:3300',
+            url: 'https://blogpgs.onrender.com/',
         }],
         security: [
             {

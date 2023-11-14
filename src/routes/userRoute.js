e070f9b4ea1b6c4dcdc7b629c20d1d225c863163
Ireme-Nyawe <akimanaj41@gmail.com>
2023-11-14 17:00:30 +0200

    add update user role to admin authorized admin

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, deleteUser, getAllUsers, postLogin, updateUser } from "../controller/userController";
+import { createUser, deleteUser, getAllUsers, postLogin, updateUser, updateUserRole } from "../controller/userController";
 import uploadfile from "../helper/multer";
 import AdminAuthorization from "../middleware/authenticate";
 import Authorization from "../middleware/authenticateUser";
@@ -8,6 +8,7 @@ const routeUser = express.Router();
 routeUser.post("/create",uploadfile.single("profile"), createUser);
 routeUser.post("/login",uploadfile.single("profile"), postLogin);
 routeUser.put("/update",Authorization,uploadfile.single("profile"),updateUser);
+routeUser.put("/update",AdminAuthorization,uploadfile.single("profile"),updateUserRole);
 routeUser.delete("/delete",Authorization,deleteUser);
 routeUser.get("/users",AdminAuthorization,getAllUsers);
 
